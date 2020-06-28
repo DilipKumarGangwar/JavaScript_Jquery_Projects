eca0d7d89ca0670fe0319778a32b8fb92323a374
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2020-06-29 02:16:17 +0530

    new4

--- Project-2/sample/finalResults.js
+++
@@ -1,7 +0,0 @@
-let TotalScore =sessionStorage.getItem("TotalScore");
-let candidateName = sessionStorage.getItem("username");
-
-document.querySelector(".score").innerHTML = TotalScore;
-document.querySelector(".candidatename").innerHTML = candidateName;
-
-console.log(TotalScore , candidateName)
