d86e84c5545c7ea20e83dd38c7b289399f0601aa
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2020-06-29 13:06:14 +0530

    tnew

--- Project-3/Countdown/counter.js
+++ Project-3/Countdown/counter.js
@@ -1,12 +1,12 @@
+//get end date till which you want the countdown to count
+
+let end = new Date("29 june 2021 10:57:00").getTime();
+
 let counter = setInterval(function(){
 
 
 //code for countdown
 
-
-//get end date till which you want the countdown to count
-
-let end = new Date("30 june 2021 13:00:00").getTime();
 
 let now = new Date();
 
@@ -27,8 +27,13 @@ let minutes =  Math.floor((timediff%(1000*60*60))/ (1000*60));
 let seconds =  Math.floor((timediff%(1000*60))/ (1000));
 
 //write on the box
-document.querySelector(".countdown").innerHTML = days +"days " + hours + "h " + minutes + "m " + seconds +"s";
+document.querySelector(".countdown").innerHTML = days +"Days " + hours + "h " + minutes + "m " + seconds +"s";
 
+if(timediff < 0)
+{
+    clearInterval(counter);
+    document.querySelector(".countdown").innerHTML = "Wait is over Now :)"   //here you can say redirect to some other page as per need
+}
 
 }
 ,1000);
